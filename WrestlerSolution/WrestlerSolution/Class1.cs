1c46ab34089caba4dcc844529effc145515c062b
agni90 <i.korotonozhkina@gmail.com>
2016-01-10 14:58:50 +0300

    Initial commit

---
+++ WrestlerSolution/WrestlerSolution/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrestlerSolution
+{
+    public class Class1
+    {
+    }
+}
