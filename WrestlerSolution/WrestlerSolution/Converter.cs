7d1b252a7f71f8087f473906b2889df8e3f92ad7
agni90 <i.korotonozhkina@gmail.com>
2016-01-12 21:03:22 +0300

    added untracked files

---
+++ WrestlerSolution/WrestlerSolution/Converter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WrestlerSolution.Models;
+using Newtonsoft.Json;
+
+namespace WrestlerSolution
+{
+    public static class Converter
+    {
+        public static SimpleWrestler JsonResponseToSimpleWrestler(string responseContent)
+        {
+            // todo add validation on response
+            SimpleWrestler simpleWrestler = JsonConvert.DeserializeObject<SimpleWrestler>(responseContent);
+            //todo check that Tst object is good
+            return simpleWrestler;
+        }
+
+        public static Wrestler JsonResponseToWrestler(string responseContent)
+        {
+            // todo add validation on response
+            Wrestler simpleWrestler = JsonConvert.DeserializeObject<Wrestler>(responseContent);
+            //todo check that Tst object is good
+            return simpleWrestler;
+        }
+
+        public static string WrestlerToJsonRequest(Wrestler wrestler)
+        {
+            string json = JsonConvert.SerializeObject(wrestler);
+            return json;
+        }
+
+        public static string SimpleWrestlerToJsonRequest(SimpleWrestler wrestler)
+        {
+            string json = JsonConvert.SerializeObject(wrestler);
+            return json;
+        }
+    }
+}
